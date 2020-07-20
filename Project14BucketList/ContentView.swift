9fa4bb934e0a7654c03c4c8ea72339336689bfff
BingyuanXu <xubingyuan123@hotmail.com>
2020-07-19 20:56:31 -0500

    add faceID and touchID

--- Project14BucketList/ContentView.swift
+++ Project14BucketList/ContentView.swift
@@ -45,37 +45,69 @@ struct ContentView: View {
     }
   }
   
+  func authenticate() {
+    let context = LAContext()
+    var error: NSError?
+    
+    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+      let reason = "Please authenticate yourself to unlock your places."
+      
+      context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+        
+        DispatchQueue.main.async {
+          if success {
+            self.isUnlocked = true
+          } else {
+            // error
+          }
+        }
+      }
+    } else {
+      // no biometrics
+    }
+  }
+  
   var body: some View {
     ZStack {
-      MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+      if isUnlocked {
+        MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+          
+          .edgesIgnoringSafeArea(.all)
+        Circle()
+          .fill(Color.blue)
+          .opacity(0.3)
+          .frame(width: 20, height: 20)
         
-        .edgesIgnoringSafeArea(.all)
-      Circle()
-        .fill(Color.blue)
-        .opacity(0.3)
-        .frame(width: 20, height: 20)
-      
-      VStack {
-        Spacer()
-        HStack {
+        VStack {
           Spacer()
-          Button(action: {
-            let newLocation = CodableMKPointAnnotation()
-            newLocation.coordinate = self.centerCoordinate
-            newLocation.title = "Example location"
-            self.locations.append(newLocation)
-            self.selectedPlace = newLocation
-            self.showingEditScreen = true
-          }) {
-            Image(systemName: "plus")
+          HStack {
+            Spacer()
+            Button(action: {
+              let newLocation = CodableMKPointAnnotation()
+              newLocation.coordinate = self.centerCoordinate
+              newLocation.title = "Example location"
+              self.locations.append(newLocation)
+              self.selectedPlace = newLocation
+              self.showingEditScreen = true
+            }) {
+              Image(systemName: "plus")
+            }
+            .padding()
+            .background(Color.black.opacity(0.75))
+            .foregroundColor(.white)
+            .font(.title)
+            .clipShape(Circle())
+            .padding(.trailing)
           }
-          .padding()
-          .background(Color.black.opacity(0.75))
-          .foregroundColor(.white)
-          .font(.title)
-          .clipShape(Circle())
-          .padding(.trailing)
         }
+      } else {
+        Button("Unlock Places") {
+          self.authenticate()
+        }
+        .padding()
+        .background(Color.blue)
+        .foregroundColor(.white)
+        .clipShape(Capsule())
       }
     }
     .alert(isPresented: $showingPlaceDetails) {
@@ -90,6 +122,7 @@ struct ContentView: View {
     }
     .onAppear(perform: loadData)
   }
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
